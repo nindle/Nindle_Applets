0caf7b80be678f30420c1e1cb30beb25d92452bd
liuchao <1104892323@qq.com>
2022-07-15 15:32:11 +0800

    免费API

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,6 +3,8 @@ const app = getApp()
 Page({
   data: {
     imageList: [],
+    ipValue: '',
+    localIpAddress: {}
   },
   onLoad: function () {
     this.readyLoad()
@@ -21,5 +23,26 @@ Page({
 	const imageList = await app.http(url, 'get', {})
 	this.setData({imageList: imageList})
 	console.log(imageList);
-  }
+  },
+
+  // input事件
+  bindKeyInput(e) {
+    this.setData({
+      ipValue: e.detail.value
+    })
+  },
+  // 获取IP地址
+  async getIpAddress(e) {
+    let url = `https://www.yuanxiapi.cn/api/iplocation/?ip=${this.data.ipValue}`
+    const ipInfo = await app.http(url, 'get', {})
+    console.log(ipInfo);
+  },
+  // 获取本机外网IP地址
+  async getIpAddress1(e) {
+    let url = `http://pv.sohu.com/cityjson?ie=utf-8`
+    const ip = await app.http(url, 'get', {})
+    this.setData({
+      localIpAddress: JSON.parse(ip.split('=')[1].split(';')[0])
+    })
+  },
 })
